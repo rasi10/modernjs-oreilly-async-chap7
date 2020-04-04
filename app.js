c0d304a2014ade6a3e1fced97c3b598f6f4dae9d
Rafael Silva <rasilva.1986@gmail.com>
2020-04-04 14:37:54 +0200

    lect4

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-document.getElementById('button').addEventListener('click', loadData)
-function loadData(){
+document.getElementById('button1').addEventListener('click', loadCustomer)
+document.getElementById('button2').addEventListener('click', loadCustomers)
+
+//load customer
+function loadCustomer(){
     const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'data.txt', true)
+    xhr.open('GET', 'customer.json', true)
     xhr.onload = function(){        
         if(this.status === 200){
-            //console.log(this.responseText)
-            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
+            const customer = JSON.parse(this.responseText)
+            const output = `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>ID: ${customer.name}</li>
+                    <li>ID: ${customer.company}</li>
+                    <li>ID: ${customer.phone}</li>
+                </ul>
+            `
+            document.getElementById('customer').innerHTML = output;
         }
-    }
+    }    
+    xhr.send()
+}
+
+// Load customers
+function loadCustomers(){
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', 'customers.json', true)
+    xhr.onload = function(){        
+        if(this.status === 200){
+            const customers = JSON.parse(this.responseText)
+            let output = ''
+            customers.forEach(function(customer) {
+                output += `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>ID: ${customer.name}</li>
+                    <li>ID: ${customer.company}</li>
+                    <li>ID: ${customer.phone}</li>
+                </ul>
+            `
+            })
+           
+            document.getElementById('customers').innerHTML = output;
+        }
+    }    
     xhr.send()
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-## Lecture 3 - XHR Object methods and working with text
-Making a simple GET request towards a local text file and displaying it in a h1 'output' in the index page
+## Lecture 4 - Working with AJAX and JSON
+Making get requests with xhr to local json files and putting content into a div.
