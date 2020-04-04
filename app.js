dea4a65dbbc59ab3ae7b95ae747738a165ea4b40
Rafael Silva <rasilva.1986@gmail.com>
2020-04-04 17:39:01 +0200

    lect7

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-document.querySelector('.get-jokes').addEventListener('click', getJokes)
+const posts = [
+    {title: 'Post One', body: 'This is post one'}, 
+    {title: 'Post Two', body: 'This is post two'}
+]
 
-function getJokes(e){
-    const number = document.querySelector('input[type="number"]').value
-    console.log(number)
-
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true)
-    xhr.onload = function(){
-        if(this.status ===200){
-            const response = JSON.parse(this.responseText)
-            // console.log(response)
-            let output = '';
-            if (response.type === 'success'){
-                response.value.forEach(joke => {
-                    output += `<li>${joke.joke}</li>`
-                });
-            } else {
-                output += '<li>Something went wrong </li>'
-            }
-            document.querySelector('.jokes').innerHTML = output;
-        }
-    }
-
-    xhr.send()
-
-    e.preventDefault()
+function createPost(post, callback){
+    setTimeout(function(){
+        posts.push(post)
+        callback()
+    }, 2000)
 }
+
+function getPosts(){
+    setTimeout(function(post) {
+        let output = ''
+        posts.forEach(function(post){
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output
+
+    }, 1000);
+}
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts)
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
-## Lecture 5 -Data from an External API - Chuck Norris Project
-Making requests with xhr towards an external API - The chuck norris joke API http://api.icndb.com/jokes/random/
+## Lecture 7 - Callback functions
