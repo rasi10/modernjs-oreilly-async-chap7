2aeb7f408fab87662dc4315dd2653ce1f268a2e9
Rafael Silva <rasilva.1986@gmail.com>
2020-04-06 20:28:17 +0200

    lect010

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
-const http = new easyHTTP
+const posts = [
+    {title: 'Post One', body: 'This is post one'}, 
+    {title: 'Post Two', body: 'This is post two'}
+]
 
-//Get posts
-/* const response = http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(posts)
-    }
-})
- */
-
-// Create data
-/* const data = {
-    title: 'Custom Post', 
-    body: 'This is a custom post'
+function createPost(post){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            posts.push(post)  
+            const error = true
+            if (!error){
+                resolve() 
+            } else {
+                reject('Something went wrong...') 
+            }
+        }, 2000)
+    })
 }
 
-//Create post
-http.post('https://jsonplaceholder.typicode.com/posts/', data, function(err, post){
-    if(err){
-        console.log(err)
-    } else {
-        console.log(post)
-    }
+function getPosts(){
+    setTimeout(function(post) {
+        let output = ''
+        posts.forEach(function(post){
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output
 
-}) */
-
-//Update a post
-/* const data = {
-    title: 'Custom Post', 
-    body: 'This is a custom post'
+    }, 1000);
 }
 
-http.put('https://jsonplaceholder.typicode.com/posts/1',data, function(err, post){
-    if(err){
-        console.log(err)
-    } else {
-        console.log(post)
-    }
-}) */
-
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(response)
-    }
+createPost({title: 'Post Three', body: 'This is post three'})
+.then(getPosts)
+.catch(function(err){
+    console.log(err)
 })
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
-## Lecture 9 - Custom http library easyhttp (Ajax and callback) - PART II
-Second and final part of Easyhttp using prototype pattern. The API used was https://jsonplaceholder.typicode.com/posts and the methods implemented in this part are POST, PUT and DELETE.
+## Lecture 10 - Example of ES6 Promises
