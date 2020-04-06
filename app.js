97de1b89470bbd2bfd67c1becab4d5454f0748c5
Rafael Silva <rasilva.1986@gmail.com>
2020-04-06 21:10:30 +0200

    lect11

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
-const posts = [
-    {title: 'Post One', body: 'This is post one'}, 
-    {title: 'Post Two', body: 'This is post two'}
-]
+document.getElementById('button1').addEventListener('click', getText)
+document.getElementById('button2').addEventListener('click', getJson)
+document.getElementById('button3').addEventListener('click', getExternal)
 
-function createPost(post){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            posts.push(post)  
-            const error = true
-            if (!error){
-                resolve() 
-            } else {
-                reject('Something went wrong...') 
-            }
-        }, 2000)
-    })
-}
 
-function getPosts(){
-    setTimeout(function(post) {
-        let output = ''
-        posts.forEach(function(post){
-            output += `<li>${post.title}</li>`
+//Get local text file data
+function getText() {
+    fetch('test.txt')
+        .then(function (res) {
+            return res.text()
         })
-        document.body.innerHTML = output
-
-    }, 1000);
+        .then(function (data) {
+            console.log(data)
+            document.getElementById('output').innerHTML = data
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 }
 
-createPost({title: 'Post Three', body: 'This is post three'})
-.then(getPosts)
-.catch(function(err){
-    console.log(err)
-})
+//Get local json file data
+function getJson() {
+    fetch('posts.json')
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            let output = '';
+            data.forEach(function(post) {
+                output += `<li>${post.title}</li>`
+            });
+
+            document.getElementById('output').innerHTML = output
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+}
+
+//Get from external API
+function getExternal() {
+    fetch('https://api.github.com/users')
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            let output = '';
+            data.forEach(function(user) {
+                output += `<li>${user.login}</li>`
+            });
+
+            document.getElementById('output').innerHTML = output
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-## Lecture 10 - Example of ES6 Promises
+## Lecture 11 - The fetch API
+Using the fetch API to make get requests towards a local text file, a local json file and an external API.
